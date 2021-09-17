4624ce20d21dbeab5ead7d7dc28a37a1fa77954c
Paritosh7 <paritosh7.sahni@gmail.com>
2021-09-17 16:46:15 +0530

    programmatically-redirect

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./lessons/passing-props/App";
+import App from "./lessons/programatically-navigate/imperitive-history/App";
 
 ReactDOM.render(
   <React.StrictMode>
